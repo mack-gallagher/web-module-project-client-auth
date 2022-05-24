da0ca406093abcfafb129aa81f3fbdcf73543234
Mack Gallagher <wingsonthebus@gmail.com>
2022-05-24 16:27:02 -0500

    get login component and header for friends list displaying

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import Login from './components/login';
+import FriendsList from './components/FriendsList';
 
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <div className="header-nav">
+        <h2>Friends Database</h2>
+        <nav>
+          <Link to='/'>Login.</Link>
+          <Link to='/friends'>Friends.</Link>
+          <Link to='/friends/add'>Addfriend.</Link>
+          <Link to='/logout'>Logout.</Link>
+        </nav>
+      </div>
+
+      <Route exact path='/'>
+        <Login />
+      </Route>
+
+      <Route path='/friends'>
+        <FriendsList />
+      </Route>
+
     </div>
   );
 }
